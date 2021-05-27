1b5614a7b3d7ad11388d150bab2b90932110b3a1
Maxime Fabas <fabas@lemonde.fr>
2021-05-27 14:13:32 +0200

    app - src/index.js - prepare for prerendering

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render, hydrate } from 'react-dom'
 import App from './App'
 import 'whatwg-fetch'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('lm-app-root')
-)
+const rootNodeId: string = 'lm-app-root'
+const rootNode: HTMLElement|null = document.getElementById(rootNodeId)
+const rendered = <React.StrictMode><App /></React.StrictMode>
+
+if (rootNode === null) console.error(`App root node '#${rootNodeId}' not found.`)
+else if (rootNode.hasChildNodes()) hydrate(rendered, rootNode)
+else render(rendered, rootNode)
