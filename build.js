c82604fb6efe1ef9751f7569ae8eb6745b4544a3
Maxime Fabas <fabas@lemonde.fr>
2021-05-27 14:09:21 +0200

    build - prettify outputs and add standard and git status checks in process

--- build.js
+++ build.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-template-curly-in-string */
+
 const _exec = require('child_process').exec
 const promisify = require('util').promisify
 const exec = promisify(_exec)
@@ -6,37 +8,46 @@ build()
 
 async function build () {
   try {
-    await cmd(`echo "\n🧹  Removing previous builds...\n"`)
-    await cmd(`rm -rfv build build.zip`)
+    await cmd('echo "\n👀 $(tput bold)Running standard.js...$(tput sgr0)\n"')
+    await laxcmd('npx ts-standard --verbose | snazzy')
 
-    await cmd(`echo "\n🛠  Building app...\n"`)
-    await cmd(`INLINE_RUNTIME_CHUNK=false GENERATE_SOURCEMAP=false SKIP_PREFLIGHT_CHECK=true react-scripts build`)
-    
+    await cmd('echo "\n📡 $(tput bold)Checking git status...$(tput sgr0)\n"')
+    await cmd('git status')
+
+    await cmd('echo "\n🧹 $(tput bold)Removing previous builds...$(tput sgr0)\n"')
+    await cmd('rm -rfv build build.zip')
+
+    await cmd('echo "\n🛠  $(tput bold)Building the app...$(tput sgr0)\n"')
+    await cmd('INLINE_RUNTIME_CHUNK=false GENERATE_SOURCEMAP=false SKIP_PREFLIGHT_CHECK=true react-scripts build')
+
     if (process.argv[2] === '--onefile') {
-      await cmd(`echo "\n🤖 Creating gulpfile.js ...\n"`)
-      await cmd(`rm -rfv gulpfile.js`)
-      await cmd(`echo "const gulp = require('gulp')" >> gulpfile.js`)
-      await cmd(`echo "const inlinesource = require('gulp-inline-source')" >> gulpfile.js`)
-      await cmd(`echo "const replace = require('gulp-replace')" >> gulpfile.js`)
-      await cmd(`echo "gulp.task('default', () => gulp" >> gulpfile.js`)
-      await cmd(`echo "  .src('./build/*.html')" >> gulpfile.js`)
-      await cmd(`echo "  .pipe(replace('.js\\"></script>', '.js\\" inline></script>'))" >> gulpfile.js`)
-      await cmd(`echo "  .pipe(replace('rel=\\"stylesheet\\">', 'rel=\\"stylesheet\\" inline>'))" >> gulpfile.js`)
-      await cmd(`echo "  .pipe(inlinesource())" >> gulpfile.js`)
-      await cmd(`echo "  .pipe(gulp.dest('./build')))" >> gulpfile.js`)
-      await cmd(`echo gulpfile.js`)
-      
-      await cmd(`echo "\n📦 Bundling everything inside index.html...\n"`)
-      await cmd(`npx gulp`)
-      
-      await cmd(`echo "\n🧽 Removing bundled files...\n"`)
-      await cmd(`rm -rfv gulpfile.js build/static build/asset-manifest.json`)
+      await cmd('echo "\n🤖 $(tput bold)Creating gulpfile.js ...$(tput sgr0)\n"')
+      await cmd('rm -rfv gulpfile.js')
+      await cmd('echo "const gulp = require(\'gulp\')" >> gulpfile.js')
+      await cmd('echo "const inlinesource = require(\'gulp-inline-source\')" >> gulpfile.js')
+      await cmd('echo "const replace = require(\'gulp-replace\')" >> gulpfile.js')
+      await cmd('echo "gulp.task(\'default\', () => gulp" >> gulpfile.js')
+      await cmd('echo "  .src(\'./build/*.html\')" >> gulpfile.js')
+      await cmd('echo "  .pipe(replace(\'.js\\"></script>\', \'.js\\" inline></script>\'))" >> gulpfile.js')
+      await cmd('echo "  .pipe(replace(\'rel=\\"stylesheet\\">\', \'rel=\\"stylesheet\\" inline>\'))" >> gulpfile.js')
+      await cmd('echo "  .pipe(inlinesource())" >> gulpfile.js')
+      await cmd('echo "  .pipe(gulp.dest(\'./build\')))" >> gulpfile.js')
+      await cmd('cat gulpfile.js')
+
+      await cmd('echo "\n📦 $(tput bold)Bundling everything inside index.html...$(tput sgr0)\n"')
+      await cmd('npx gulp')
+
+      await cmd('echo "\n🧽 $(tput bold)Removing bundled files...$(tput sgr0)\n"')
+      await cmd('rm -rfv gulpfile.js build/static')
     }
-    
-    await cmd(`echo "\n🤐 Zipping the build...\n"`)
-    await cmd(`zip -r build.zip build`)
-    
-    await cmd(`echo "\n🍸 That\'s all good my friend!\n"`)
+
+    await cmd('echo "\n🤷‍♀️ $(tput bold)Removing asset-manifest.json...$(tput sgr0)\n"')
+    await cmd('rm -rfv build/asset-manifest.json')
+
+    await cmd('echo "\n🤐 $(tput bold)Zipping the build...$(tput sgr0)\n"')
+    await cmd('zip -r build.zip build')
+
+    await cmd('echo "\n🍸 $(tput bold)That\'s all good my friend!$(tput sgr0)\n"')
   } catch (err) {
     console.log('\n', err)
     process.exit(1)
@@ -46,5 +57,14 @@ async function build () {
 async function cmd (line) {
   const { stdout, stderr } = await exec(line)
   if (stderr) throw stderr
-  if (stdout) console.log(stdout)
+  else if (stdout) console.log(stdout)
+}
+
+async function laxcmd (line) {
+  try {
+    await cmd(line)
+  } catch (err) {
+    console.log(err.stdout)
+    console.log(err.stderr)
+  }
 }
