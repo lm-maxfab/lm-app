81d55d42bc0e7be4d7096dcaebc43be49086edcd
Maxime Fabas <fabas@lemonde.fr>
2021-09-01 14:29:43 +0200

    4-2109-drogues - clean unnecessary stuff

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -26,9 +26,6 @@ class App extends React.Component<Props> {
     )
     const inlineStyle = {
       ...props.style,
-      fontSize: 'clamp(18px, calc(2 * var(--vw)), 40px)',
-      lineHeight: 1.75,
-      padding: 'clamp(32px, calc(8.33 * var(--vw)), 120px)',
       marginTop: 'var(--len-nav-height)'
     }
 
@@ -37,19 +34,7 @@ class App extends React.Component<Props> {
         id={config.project_short_name}
         className={classes}
         style={inlineStyle}>
-        <p style={{ fontFamily: 'var(--ff-the-antiqua-b)', fontWeight: 500, fontStyle: 'normal' }}>The Antiqua B, 500, normal</p>
-        <p style={{ fontFamily: 'var(--ff-the-antiqua-b)', fontWeight: 500, fontStyle: 'italic' }}>The Antiqua B, 500, italic</p>
-        <p style={{ fontFamily: 'var(--ff-the-antiqua-b)', fontWeight: 700, fontStyle: 'normal' }}>The Antiqua B, 700, normal</p>
-        <p style={{ fontFamily: 'var(--ff-the-antiqua-b)', fontWeight: 700, fontStyle: 'italic' }}>The Antiqua B, 700, italic</p>
-        <p style={{ fontFamily: 'var(--ff-the-antiqua-b)', fontWeight: 800, fontStyle: 'normal' }}>The Antiqua B, 800, normal</p>
-        <p style={{ fontFamily: 'var(--ff-marr-sans)', fontWeight: 400, fontStyle: 'normal' }}>Marr Sans, 400, normal</p>
-        <p style={{ fontFamily: 'var(--ff-marr-sans)', fontWeight: 500, fontStyle: 'normal' }}>Marr Sans, 500, normal</p>
-        <p style={{ fontFamily: 'var(--ff-marr-sans)', fontWeight: 600, fontStyle: 'normal' }}>Marr Sans, 600, normal</p>
-        <p style={{ fontFamily: 'var(--ff-marr-sans)', fontWeight: 700, fontStyle: 'normal' }}>Marr Sans, 700, normal</p>
-        <p style={{ fontFamily: 'var(--ff-marr-sans-condensed)', fontWeight: 500, fontStyle: 'normal' }}>Marr Sans, 500, normal</p>
-        <p style={{ fontFamily: 'var(--ff-marr-sans-condensed)', fontWeight: 600, fontStyle: 'normal' }}>Marr Sans, 600, normal</p>
-        <p style={{ fontFamily: 'var(--ff-marr-sans-condensed)', fontWeight: 700, fontStyle: 'normal' }}>Marr Sans, 700, normal</p>
-        <p style={{ fontFamily: 'var(--ff-fette-engschrift)', fontWeight: 500, fontStyle: 'normal' }}>Fette Engschrift, 500, normal</p>
+        App
       </div>
     )
   }
