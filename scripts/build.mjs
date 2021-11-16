2d7ffc7df7c19ad9fc5204ce6afec82a80a30d32
Maxime Fabas <fabas@lemonde.fr>
2021-11-16 17:11:15 +0100

    BUILD - v0.0.0-stable-1 - azd

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -48,6 +48,7 @@ async function build () {
     await exec(`git commit -m "BUILD - ${versionName} - ${buildDescription}"`)
     const pushResult = await exec(`git push origin ${branch}`)
     console.log(pushResult)
+    console.log('lol')
 
     // // Move all buildable to .build
     // if ((await ROOT.get('.build') === undefined)) await ROOT.mkdir('.build')
