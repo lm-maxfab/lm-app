d1c681fc14f461bb6b530b888f8ed16cdc09d7fb
Maxime Fabas <fabas@lemonde.fr>
2021-12-02 14:04:21 +0100

    6-2111-destinations22 - update static/dev/scripts/config.js

--- static/dev/scripts/config.js
+++ static/dev/scripts/config.js
@@ -1,20 +1,11 @@
 /* Generated via /scripts/predev.mjs */
 window.LM_APP_CONFIG = {
-<<<<<<< HEAD
   "assets_root_url": "https://assets-decodeurs.lemonde.fr/redacweb/6-2111-destinations22-assets",
   "sheetbases": {
     "production": "https://assets-decodeurs.lemonde.fr/sheets/cjZyvjYsc05-NCuPSpey9wviliXAPA_745",
     "staging": "https://assets-decodeurs.lemonde.fr/sheets/cjZyvjYsc05-NCuPSpey9wviliXAPA_746",
     "testing": "https://assets-decodeurs.lemonde.fr/sheets/cjZyvjYsc05-NCuPSpey9wviliXAPA_747",
     "developpment": "https://assets-decodeurs.lemonde.fr/sheets/cjZyvjYsc05-NCuPSpey9wviliXAPA_748"
-=======
-  "assets_root_url": "https://assets-decodeurs.lemonde.fr/redacweb/8-2111-photos21-assets",
-  "sheetbases": {
-    "production": "https://assets-decodeurs.lemonde.fr/sheets/lcHZvwZYA1b7xKmbpx6gCh030ty_Eg_758",
-    "staging": "https://assets-decodeurs.lemonde.fr/sheets/lcHZvwZYA1b7xKmbpx6gCh030ty_Eg_759",
-    "testing": "https://assets-decodeurs.lemonde.fr/sheets/lcHZvwZYA1b7xKmbpx6gCh030ty_Eg_760",
-    "developpment": "https://assets-decodeurs.lemonde.fr/sheets/lcHZvwZYA1b7xKmbpx6gCh030ty_Eg_761"
->>>>>>> master
   },
   "env": "developpment"
 }
