03c710dd5793fbc0c6717e54d77e9b24121b20b5
Maxime Fabas <fabas@lemonde.fr>
2021-12-08 11:09:58 +0100

    6-2111-destinations22 - Add Credits component

---
+++ src/App/components/Credits/index.tsx
@@ -0,0 +1,35 @@
+import { Component, JSX } from 'preact'
+import bem from '../../../modules/le-monde/utils/bem'
+
+import './styles.scss'
+
+interface Props {
+  className?: string
+  style?: JSX.CSSProperties
+}
+
+class Credits extends Component<Props, {}> {
+  static clss = 'dest22-credits'
+  clss = Credits.clss
+
+  /* * * * * * * * * * * * * * *
+   * RENDER
+   * * * * * * * * * * * * * * */
+  render (): JSX.Element|null {
+    const { props } = this
+
+    /* Classes and style */
+    const wrapperClasses = bem(props.className ?? '').block(this.clss)
+    const wrapperStyle: JSX.CSSProperties = { ...props.style }
+
+    /* Display */
+    return (
+      <div className={wrapperClasses.value} style={wrapperStyle}>
+        TSX component Credits.
+      </div>
+    )
+  }
+}
+
+export type { Props }
+export default Credits
