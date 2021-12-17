1ee1314ab24c6e99ecf24038a559f6fe0ea08495
Maxime Fabas <fabas@lemonde.fr>
2021-12-17 18:13:48 +0100

    BUILD - 6-2111-destinations22 - v1.0.0-stable-1

--- src/apps/Footer/index.tsx
+++ src/apps/Footer/index.tsx
@@ -3,11 +3,13 @@ import './styles.scss'
 import { SheetBase } from '../../modules/le-monde/utils/sheet-base'
 import bem from '../../modules/le-monde/utils/bem'
 import getViewportDimensions from '../../modules/le-monde/utils/get-viewport-dimensions'
-import { Destination as DestinationType } from '../types'
+import { Destination as DestinationType, GeneralSettingsData } from '../types'
 import DestinationWindow from '../components/DestinationWindow'
 import DestinationSupertitle from '../components/DestinationSupertitle'
 import DestinationTitle from '../components/DestinationTitle'
 import DestinationOpener from '../components/DestinationOpener'
+import Img from '../../modules/le-monde/components/Img'
+import longformImgUrl from './assets/longform-img.jpg'
 
 interface Props {
   className?: string
@@ -27,6 +29,7 @@ class Footer extends Component<Props, {}> {
     // Logic
     const { navHeight } = getViewportDimensions()
     const destinations = (props.sheetBase?.collection('destinations').value ?? []) as unknown as DestinationType[]
+    const generalSettings = (props.sheetBase?.collection('general_settings').value ?? [])[0] as unknown as GeneralSettingsData
 
     // Assign classes
     const wrapperClasses = bem(props.className ?? '').block('lm-app').block(this.clss)
@@ -56,23 +59,36 @@ class Footer extends Component<Props, {}> {
           Les autres voyages du <em>«&nbsp;Monde&nbsp;»</em>
         </h3>
         <div className={bem(this.clss).elt('destinations').value}>
-          {destinations.map((destination) => {
-            return <a
-              href={destination.article_url}
-              className={bem(this.clss).elt('destination').value}>
-              <DestinationWindow  
-                fixedImage={false}
-                forceBgCover={true}
-                photoUrl={destination.main_photo_url_alt}
-                shape={destination.shape} />
-              <DestinationSupertitle
-                textColor='#0E1B4C'
-                content={destination.supertitle} />
-              <DestinationTitle
-                textColor='#0E1B4C'
-                content={destination.title} />
-            </a>
-          })}
+          <a
+            href={generalSettings.longform_url}
+            className={bem(this.clss).elt('destination').mod('starter').value}>
+            <Img src={longformImgUrl} />
+            <DestinationTitle
+              textColor='#0E1B4C'
+              content='Découvrez les 20 destinations 2022' />
+          </a>
+          {destinations
+            .filter(({ article_url: url }) => {
+              if (typeof url !== 'string' || url.length <= 0) return false
+              return true
+            }).map((destination) => {
+              return <a
+                href={destination.article_url}
+                className={bem(this.clss).elt('destination').value}>
+                <DestinationWindow  
+                  fixedImage={false}
+                  forceBgCover={true}
+                  photoUrl={destination.main_photo_url_alt}
+                  shape={destination.shape} />
+                <DestinationSupertitle
+                  textColor='#0E1B4C'
+                  content={destination.supertitle} />
+                <DestinationTitle
+                  textColor='#0E1B4C'
+                  content={destination.title} />
+              </a>
+            })
+          }
         </div>
       </div>
     )
