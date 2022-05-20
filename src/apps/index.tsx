8ecb5d3e689ed78134674670ecb49fa1ccd2e805
Maxime Fabas <fabas@lemonde.fr>
2022-05-20 11:49:01 +0200

    BUILD - 14-2205-xjg-files - v1.2.1-stable-0

--- src/apps/index.tsx
+++ src/apps/index.tsx
@@ -2,13 +2,15 @@ import Art1Cover from './Art1Cover'
 import Art1Snip1 from './Art1Snip1'
 import Art1Snip2 from './Art1Snip2'
 import Art2Cover from './Art2Cover'
+import Art2Snip1 from './Art2Snip1'
 import { AppNodeMap } from '../modules/utils/render-app'
 
 const appsNodes: AppNodeMap[] = [
   { app: Art1Cover, selector: '.xjg-files-art-1-cover-root' },
   { app: Art1Snip1, selector: '.xjg-files-art-1-snip-1-root' },
   { app: Art1Snip2, selector: '.xjg-files-art-1-snip-2-root' },
-  { app: Art2Cover, selector: '.xjg-files-art-2-cover-root' }
+  { app: Art2Cover, selector: '.xjg-files-art-2-cover-root' },
+  { app: Art2Snip1, selector: '.xjg-files-art-2-snip-1-root' }
 ]
 
 export default appsNodes
