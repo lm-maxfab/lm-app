6a9a439c65c2045abf010db0da7c5fb95f5a8af6
Maxime Fabas <fabas@lemonde.fr>
2022-05-24 16:12:05 +0200

    BUILD - 15-2205-cannes - v1.0.0-stable-0

--- src/apps/index.tsx
+++ src/apps/index.tsx
@@ -1,16 +1,8 @@
-import Art1Cover from './Art1Cover'
-import Art1Snip1 from './Art1Snip1'
-import Art1Snip2 from './Art1Snip2'
-import Art1Snip3 from './Art1Snip3'
-import Art2Cover from './Art2Cover'
+import Longform from './Longform'
 import { AppNodeMap } from '../modules/utils/render-app'
 
 const appsNodes: AppNodeMap[] = [
-  { app: Art1Cover, selector: '.xjg-files-art-1-cover-root' },
-  { app: Art1Snip1, selector: '.xjg-files-art-1-snip-1-root' },
-  { app: Art1Snip2, selector: '.xjg-files-art-1-snip-2-root' },
-  { app: Art1Snip3, selector: '.xjg-files-art-1-snip-3-root' },
-  { app: Art2Cover, selector: '.xjg-files-art-2-cover-root' }
+  { app: Longform, selector: '.cannes-longform-root' }
 ]
 
 export default appsNodes
