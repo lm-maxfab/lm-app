31d89c7ab2bdecf36d7c16d992cf4326afacc4c9
Maxime Fabas <fabas@lemonde.fr>
2022-07-22 16:31:24 +0200

    BUILD - 21-2207-syrie - v0.2.0-stable-1

--- src/apps/index.tsx
+++ src/apps/index.tsx
@@ -1,10 +1,12 @@
 import Cover from './Cover'
 import Snippet from './Snippet'
+import Snippet2 from './Snippet2'
 import { AppNodeMap } from '../modules/utils/render-app'
 
 const appsNodes: AppNodeMap[] = [
   { app: Cover, selector: '.syrie-cover-root' },
-  { app: Snippet, selector: '.syrie-snippet-root' }
+  { app: Snippet, selector: '.syrie-snippet-root' },
+  { app: Snippet2, selector: '.syrie-snippet-2-root' }
 ]
 
 export default appsNodes
