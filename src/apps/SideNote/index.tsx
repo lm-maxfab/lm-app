4e362464b4aced83bacda1665efadcfae4fc94ad
Elsa <edelmas@sempo10009.glm.lan>
2022-11-02 14:40:45 +0100

    sidenote

---
+++ src/apps/SideNote/index.tsx
@@ -0,0 +1,47 @@
+import { Component, JSX } from 'preact'
+import appWrapper, { InjectedProps } from '../../modules/utils/app-wrapper-HOC'
+import Marker from '../components/Marker'
+import Arrow from '../components/Arrow'
+import bem from '../../modules/utils/bem'
+import { GeneralData } from '../types'
+import './styles.scss'
+
+interface Props extends InjectedProps {}
+interface State {}
+
+class SideNote extends Component<Props, State> {
+  static clss: string = 'mondial-sidenote'
+  clss = SideNote.clss
+
+  /* * * * * * * * * * * * * * *
+   * RENDER
+   * * * * * * * * * * * * * * */
+  render (): JSX.Element {
+    const { props } = this
+
+    const generalData = ((props.sheetBase?.collection('general').value ?? []) as unknown as GeneralData[])
+    const sideNoteText = generalData[0]?.sidenote
+    
+    // Assign classes and styles
+    const wrapperClasses = bem(props.className).block(this.clss)
+    const wrapperStyle: JSX.CSSProperties = {
+      ...props.style,
+    }
+
+    // Display
+    return <div
+      style={wrapperStyle}
+      className={wrapperClasses.value}>
+
+      <Marker width='93' color='#fff'></Marker>
+
+      <span>{sideNoteText ?? 'Quelles sont les équipes de la coupe du monde ?'}</span>
+
+      <Arrow color='#fff'></Arrow>
+      
+    </div>
+  }
+}
+
+export type { Props, SideNote }
+export default appWrapper(SideNote)
--- src/apps/index.tsx
+++ src/apps/index.tsx
@@ -3,12 +3,14 @@ import Longform from './Longform'
 import Header from './Header'
 import Cover from './Cover'
 import Footer from './Footer'
+import SideNote from './SideNote'
 
 const appsNodes: AppNodeMap[] = [
   { app: Longform, selector: '.mondial-longform-root' },
   { app: Header, selector: '.mondial-header-root' },
   { app: Cover, selector: '.mondial-cover-root' },
   { app: Footer, selector: '.mondial-footer-root' },
+  { app: SideNote, selector: '.mondial-sidenote-root' },
 ]
 
 export default appsNodes
