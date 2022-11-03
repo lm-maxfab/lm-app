64c403481905bde5c6615eca3ce528caf4def63f
Elsa <edelmas@sempo10009.glm.lan>
2022-11-03 11:26:40 +0100

    wip inté cover

--- src/apps/Cover/index.tsx
+++ src/apps/Cover/index.tsx
@@ -1,34 +1,67 @@
 import { Component, JSX } from 'preact'
-import DemoPage from '../../modules/layouts/DemoPage'
 import appWrapper, { InjectedProps } from '../../modules/utils/app-wrapper-HOC'
 import bem from '../../modules/utils/bem'
+import { TeamData } from '../types'
+import Gradient from '../components/Gradient'
+import Circle from '../components/Circle'
 import './styles.scss'
 
-interface Props extends InjectedProps {}
-interface State {}
+import getConfig from '../../modules/utils/get-config'
 
-class Longform extends Component<Props, State> {
-  static clss: string = 'sable-longform'
-  clss = Longform.clss
+const config = getConfig()
+
+interface Props extends InjectedProps { }
+interface State { }
+
+const appIdNode = document.querySelector('.lm-app-id') as HTMLElement
+const appId = appIdNode?.innerText
+
+class Cover extends Component<Props, State> {
+  static clss: string = 'mondial-cover'
+  clss = Cover.clss
 
   /* * * * * * * * * * * * * * *
    * RENDER
    * * * * * * * * * * * * * * */
-  render (): JSX.Element {
+  render(): JSX.Element {
     const { props } = this
+
+    const teamsData = ((props.sheetBase?.collection('teams').value ?? []) as unknown as TeamData[])
+    const coverTeam = teamsData.find(team => team.iso?.toLowerCase() === appId.toLowerCase())
 
     // Assign classes and styles
     const wrapperClasses = bem(props.className).block(this.clss)
-    const wrapperStyle: JSX.CSSProperties = { ...props.style }
+    const wrapperStyle: JSX.CSSProperties = {
+      ...props.style,
+      ['--mondial-main-color']: '#3E001F',
+    }
+
+    const className = bem(this.clss);
+
+    console.log(coverTeam)
+
+    const imgPlayer = `${config?.assets_root_url}/player-${coverTeam?.iso?.toLowerCase()}.png`
 
     // Display
     return <div
       style={wrapperStyle}
       className={wrapperClasses.value}>
-      <DemoPage />
+
+      <div className={className.elt('circle').value}>
+        <Circle></Circle>
+      </div>
+
+      <div className={className.elt('gradient').value}>
+        <Gradient></Gradient>
+        <div className={className.elt('circle').mod('overlay').value}>
+          <Circle></Circle>
+        </div>
+      </div>
+
+      <img className={className.elt('player').value} src={imgPlayer}></img>
     </div>
   }
 }
 
-export type { Props, Longform }
-export default appWrapper(Longform)
+export type { Props, Cover }
+export default appWrapper(Cover)
