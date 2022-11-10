57d4be38e5d4f61328515abb08ec56b245405ced
Elsa <edelmas@sempo10009.glm.lan>
2022-11-10 18:47:36 +0100

    clean

--- src/apps/Longform/index.tsx
+++ src/apps/Longform/index.tsx
@@ -2,14 +2,12 @@ import { Component, JSX } from 'preact'
 import GuideCover from '../components/GuideCover'
 import InfoText from '../components/InfoText'
 import GroupBlock from '../components/Group/GroupBlock'
-import TeamImage from '../components/Team/TeamImage'
+import ArticleHeader from '../../modules/components/ArticleHeader'
+import ArticleCredits from '../../modules/components/ArticleCredits'
 import appWrapper, { InjectedProps } from '../../modules/utils/app-wrapper-HOC'
 import bem from '../../modules/utils/bem'
 import { TeamData, GeneralData } from '../types'
 import './styles.scss'
-import ArticleHeader from '../../modules/components/ArticleHeader'
-import ArticleCredits from '../../modules/components/ArticleCredits'
-import MyComp from '../components/MyComp'
 
 interface Props extends InjectedProps { }
 interface State { }
