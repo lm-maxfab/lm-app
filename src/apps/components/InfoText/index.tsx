d6c73a9a1d1af82d699f09a1dbecbe98ae03286e
Elsa <edelmas@sempo10009.glm.lan>
2022-11-10 14:54:33 +0100

    ajout infos résultats + calendrier

---
+++ src/apps/components/InfoText/index.tsx
@@ -0,0 +1,24 @@
+import { Component, VNode } from 'preact'
+import bem from '../../../modules/utils/bem'
+import './styles.scss'
+
+import getConfig from '../../../modules/utils/get-config'
+
+const config = getConfig()
+
+type Props = {
+  content?: VNode | string,
+}
+
+type State = {
+}
+
+export const className = bem('mondial-info')
+
+export default class Flag extends Component<Props, State> {
+  render () {
+    return <div className={className.value}>
+        {this.props.content}
+    </div>
+  }
+}
