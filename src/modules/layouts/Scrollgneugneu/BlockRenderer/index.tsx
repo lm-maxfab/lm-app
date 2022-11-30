5ae8e4add509fa654d96f46f8360aee8e4bd97f2
Maxime Fabas <fabas@lemonde.fr>
2022-11-30 12:18:30 +0100

    scrllgngn - external modules load, init and update with context well

--- src/modules/layouts/Scrollgneugneu/BlockRenderer/index.tsx
+++ src/modules/layouts/Scrollgneugneu/BlockRenderer/index.tsx
@@ -18,13 +18,22 @@ type Props = {
 
 type State = {
   moduleLoading: boolean
-  moduleErrors: any
+  moduleLoadErrors: Error|null
   moduleData: ModuleData|null
+  moduleTarget: HTMLElement|null
 }
 
-type StateSetter = ((s: State) => State|null)|Partial<State>
+type StateSetter = ((s: State) => (State|null))|Partial<State>
 
 export default class BlockRenderer extends Component<Props, State> {
+  $moduleWrapper: HTMLDivElement|null = null
+  state: State = {
+    moduleLoading: false,
+    moduleLoadErrors: null,
+    moduleData: null,
+    moduleTarget: null
+  }
+
   constructor (props: Props) {
     super(props)
     this.aSetState = this.aSetState.bind(this)
@@ -32,59 +41,117 @@ export default class BlockRenderer extends Component<Props, State> {
     this.initModule = this.initModule.bind(this)
     this.loadInitModule = this.loadInitModule.bind(this)
     this.updateModule = this.updateModule.bind(this)
+    this.attachModuleTarget = this.attachModuleTarget.bind(this)
   }
 
-  componentDidMount () {
-    const { type, content } = this.props
-    if (type !== 'module' || content === undefined) return
-    this.loadInitModule ()
+  componentDidMount (): void {
+    const { props, loadInitModule, attachModuleTarget } = this
+    const { type } = props
+    if (type !== 'module') return;
+    loadInitModule()
+    attachModuleTarget()
   }
 
-  async aSetState (stateSetter: StateSetter) {
+  componentDidUpdate(pProps: Readonly<Props>): void {
+    const { props, loadInitModule, updateModule, attachModuleTarget } = this
+    const { type, content } = props
+    const { type: pType, content: pContent } = pProps
+    if (type !== pType || content !== pContent) loadInitModule()
+    else updateModule()
+    attachModuleTarget()
+  }
+
+  async aSetState (stateSetter: StateSetter): Promise<StateSetter> {
     return new Promise(resolve => {
-      this.setState(
-        stateSetter,
-        () => resolve(true)
-      )
+      const resolver = () => resolve(stateSetter)
+      return this.setState(stateSetter, resolver)
     })
   }
 
   async loadModule () {
-    await this.aSetState(curr => ({
-      ...curr,
-      moduleLoading: true,
-      moduleErrors: null,
-      moduleData: null
-    }))
+    const { props, state } = this
+    const { type, content } = props
+    const { moduleLoading } = state
+    if (type !== 'module' || content === undefined) return;
+    if (moduleLoading) return;
+    await this.aSetState(curr => ({ ...curr, moduleLoading: true }))
+    const moduleUrl = content
+    try {
+      const importedData = (await import(moduleUrl)) as unknown
+      const importedIsNotObject = typeof importedData !== 'object'
+      const importedIsNullish = importedData === null || importedData === undefined
+      if (importedIsNotObject || importedIsNullish) throw new Error('Imported module is not an object')      
+      const importedHasInitFunc = 'init' in importedData && typeof importedData.init === 'function'
+      const importedHasUpdateFunc = 'update' in importedData && typeof importedData.update === 'function'
+      if (!importedHasInitFunc) throw new Error('Imported module must export a function named init')
+      if (!importedHasUpdateFunc) throw new Error('Imported module must export a function named update')
+      const moduleData = importedData as ModuleData
+      await this.aSetState(curr => ({ ...curr, moduleLoading: false, moduleLoadErrors: null, moduleData }))
+    } catch (err: unknown) {
+      let moduleLoadErrors: Error = new Error('Unknown error')
+      if (err instanceof Error) moduleLoadErrors = err
+      else if (typeof err === 'string') moduleLoadErrors = new Error(err)
+      await this.aSetState(curr => ({ ...curr, moduleLoading: false, moduleLoadErrors }))
+    } 
   }
 
   async initModule () {
-    
+    const { props, state } = this
+    const { context } = props
+    const { moduleData } = state
+    if (moduleData === null) return;
+    const moduleTarget = moduleData.init(context ?? {}) as unknown
+    const targetIsHTMLElement = moduleTarget instanceof HTMLElement
+    if (!targetIsHTMLElement) {
+      const errorStr = 'Imported module init exported function should return a HTMLElement object'
+      return await this.aSetState(curr => ({ ...curr, moduleLoadErrors: new Error(errorStr) }))
+    }
+    return await this.aSetState(curr => ({ ...curr, moduleLoadErrors: null, moduleTarget }))
   }
 
   async loadInitModule () {
-
+    await this.loadModule()
+    await this.initModule()
   }
 
   async updateModule () {
-    
+    const { props, state } = this
+    const { context } = props
+    const { moduleData, moduleTarget } = state
+    if (moduleData === null || moduleTarget === null) return;
+    moduleData.update(moduleTarget, context ?? {})
+  }
+
+  attachModuleTarget () {
+    const { state, $moduleWrapper } = this
+    const { moduleTarget } = state
+    if ($moduleWrapper === null) return;
+    if (moduleTarget === null) return;
+    $moduleWrapper.replaceChildren(moduleTarget)
   }
 
   render () {
-    const { props } = this
-    const content = props.content ?? ''
-    console.log('BLKRDR - RENDER', props.context)
-
-    switch (props.type) {
+    const { props, state } = this
+    const { type, content } = props
+    const { moduleLoading, moduleLoadErrors, moduleTarget } = state
+    switch (type) {
       case 'html':
       case undefined:
-        return <div dangerouslySetInnerHTML={{ __html: content }} />
+        return content !== undefined
+          ? <div dangerouslySetInnerHTML={{ __html: content }} />
+          : null
       case 'module':
-        return <div>
-          I am module
+        if (moduleLoading) return <div>Loading...</div>
+        else if (moduleLoadErrors !== null) return <div>
+          <strong>Module load errors:</strong>
+          <p>{moduleLoadErrors.name}</p>
+          <p>{moduleLoadErrors.message}</p>
+          <pre>{moduleLoadErrors.stack}</pre>
         </div>
+        else if (moduleTarget !== null) return <div ref={n => { this.$moduleWrapper = n }} />
+        else return null
       default:
-        return <div>Block type {props.type} is unknown</div>
+        return <div>Block type {type} is unknown</div>
     }
   }
 }
