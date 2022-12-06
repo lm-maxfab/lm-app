131d3cfe52fb32439c6bf042ed962566ba3c315b
Elsa <edelmas@sempo10009.glm.lan>
2022-12-06 16:23:44 +0100

    BUILD - v0.1.2-stable-24 - styles pour éviter de voir le footer pendant le chargement

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,10 @@ function renderApp (sheetBaseTsv?: string): void {
   silentLog('Rendering app.')
   const sheetBase = sheetBaseTsv !== undefined ? tsvToSheetBase(sheetBaseTsv) : undefined
   const appRootNode: HTMLElement|null = document.getElementById('lm-app-name')
+  if (appRootNode !== null) {
+    appRootNode.style.backgroundColor = ''
+    appRootNode.style.minHeight = ''
+  }
   if (appRootNode === null) silentLog('App root node not found.')
   else render(<App sheetBase={sheetBase} />, appRootNode)
 }
