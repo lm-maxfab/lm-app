36716b13067b3c6b7e556e55229585736b3ff7f0
Maxime Fabas <fabas@lemonde.fr>
2022-12-29 15:11:20 +0100

    scrnnlgngn - fix build blocking stuff after dependencies upgrade

--- src/modules/utils/get-current-downlink/index.ts
+++ src/modules/utils/get-current-downlink/index.ts
@@ -1,23 +1,23 @@
-interface ExtendedNavigator extends Navigator {
-  mozConnection?: NetworkInformation
-  webkitConnection?: NetworkInformation
-}
+// interface ExtendedNavigator extends Navigator {
+//   mozConnection?: NetworkInformation
+//   webkitConnection?: NetworkInformation
+// }
 
-interface ExtendedConnection extends NetworkInformation {
-  downlink?: number
-}
+// interface ExtendedConnection extends NetworkInformation {
+//   downlink?: number
+// }
 
-export type {
-  ExtendedNavigator,
-  ExtendedConnection
-}
+// export type {
+//   ExtendedNavigator,
+//   ExtendedConnection
+// }
 
 export default function getCurrentDownlink (): number|undefined {
-  const navigator = window.navigator as ExtendedNavigator|undefined
+  const navigator = window.navigator as any // ExtendedNavigator|undefined
   const connection = (
     navigator?.connection
     ?? navigator?.mozConnection
     ?? navigator?.webkitConnection
-  ) as ExtendedConnection|undefined
+  ) as any // ExtendedConnection|undefined
   return connection?.downlink
 }
