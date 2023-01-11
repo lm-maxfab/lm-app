44ed31ae7775ca7a5a9f565966bf55ee58012ed1
Elsa <edelmas@sempo10009.glm.lan>
2023-01-11 18:10:40 +0100

    modif classe racine

--- src/apps/index.tsx
+++ src/apps/index.tsx
@@ -2,7 +2,7 @@ import { AppNodeMap } from '../modules/utils/render-app'
 import Footer from './Footer'
 
 const appsNodes: AppNodeMap[] = [
-  { app: Footer, selector: '.footer-root' },
+  { app: Footer, selector: '.lm-footer-root' },
 ]
 
 export default appsNodes
