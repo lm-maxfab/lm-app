0e78aa045c8a80ab06692bf27db644f6645e8bc6
Elsa <edelmas@sempo10009.glm.lan>
2023-02-03 17:25:29 +0100

    add slots + generic class names

--- src/apps/index.tsx
+++ src/apps/index.tsx
@@ -2,12 +2,27 @@ import { AppNodeMap } from '../modules/utils/render-app'
 import LongformOf from './Longform'
 
 const appsNodes: AppNodeMap[] = [
-  { app: LongformOf('cover'), selector: '.iran-cover-slot' },
-  { app: LongformOf('slot-1'), selector: '.iran-article-slot-1' },
-  { app: LongformOf('slot-2'), selector: '.iran-article-slot-2' },
-  { app: LongformOf('slot-3'), selector: '.iran-article-slot-3' },
-  { app: LongformOf('slot-4'), selector: '.iran-article-slot-4' },
-  { app: LongformOf('slot-5'), selector: '.iran-article-slot-5' }
+  { app: LongformOf('cover'), selector: '.scrllgngn-cover-slot' },
+  { app: LongformOf('slot-1'), selector: '.scrllgngn-article-slot-1' },
+  { app: LongformOf('slot-2'), selector: '.scrllgngn-article-slot-2' },
+  { app: LongformOf('slot-3'), selector: '.scrllgngn-article-slot-3' },
+  { app: LongformOf('slot-4'), selector: '.scrllgngn-article-slot-4' },
+  { app: LongformOf('slot-5'), selector: '.scrllgngn-article-slot-5' },
+  { app: LongformOf('slot-6'), selector: '.scrllgngn-article-slot-6' },
+  { app: LongformOf('slot-7'), selector: '.scrllgngn-article-slot-7' },
+  { app: LongformOf('slot-8'), selector: '.scrllgngn-article-slot-8' },
+  { app: LongformOf('slot-9'), selector: '.scrllgngn-article-slot-9' },
+  { app: LongformOf('slot-10'), selector: '.scrllgngn-article-slot-10' },
+  { app: LongformOf('slot-11'), selector: '.scrllgngn-article-slot-11' },
+  { app: LongformOf('slot-12'), selector: '.scrllgngn-article-slot-12' },
+  { app: LongformOf('slot-13'), selector: '.scrllgngn-article-slot-13' },
+  { app: LongformOf('slot-14'), selector: '.scrllgngn-article-slot-14' },
+  { app: LongformOf('slot-15'), selector: '.scrllgngn-article-slot-15' },
+  { app: LongformOf('slot-16'), selector: '.scrllgngn-article-slot-16' },
+  { app: LongformOf('slot-17'), selector: '.scrllgngn-article-slot-17' },
+  { app: LongformOf('slot-18'), selector: '.scrllgngn-article-slot-18' },
+  { app: LongformOf('slot-19'), selector: '.scrllgngn-article-slot-19' },
+  { app: LongformOf('slot-20'), selector: '.scrllgngn-article-slot-20' }
 ]
 
 export default appsNodes
