a09c45a6b1b496a9e2253f0ae3218cf4cbbc37c4
Maxime Fabas <fabas@lemonde.fr>
2023-02-08 19:05:31 +0100

    scrllgngn-dev-env - 23/02/08

--- src/apps/Longform/index.tsx
+++ src/apps/Longform/index.tsx
@@ -1,14 +1,9 @@
 import { Component, JSX } from 'preact'
-import Scrollgneugneu, { Props as ScrollgngnProps, PropsPageData, PropsBlockData } from '../../modules/layouts/Scrollgneugneu'
+import Scrollgneugneu, { PropsPageData } from '../../modules/layouts/Scrollgneugneu'
 import appWrapper, { InjectedProps } from '../../modules/utils/app-wrapper-HOC'
 import bem from '../../modules/utils/bem'
 import './styles.scss'
-
-import {
-  GeneralSettings,
-  BlockData as BlockDataFromSheet,
-  PageData as PageDataFromSheet
-} from '../types'
+import { GeneralSettings } from '../types'
 
 interface Props extends InjectedProps {}
 interface State {}
@@ -22,171 +17,134 @@ class Longform extends Component<Props, State> {
    * * * * * * * * * * * * * * */
   render (): JSX.Element {
     const { props } = this
-    const { sheetBase } = props
-
-    const generalSettings = sheetBase?.collection('general_settings').entries[0].value as GeneralSettings|undefined
-    const blocksData = sheetBase?.collection('blocks_data').value as BlockDataFromSheet[]|undefined
-    const rawPagesData = sheetBase?.collection('pages_data').value as PageDataFromSheet[]|undefined
-    const pagesData: PropsPageData[]|undefined = rawPagesData?.map(rawPageData => {
-      const fixedBlocksData: PropsBlockData[] = []
-      rawPageData.blocksIds?.split(',').map(name => {
-        const blockId = name.trim()
-        const theActualBlock = blocksData?.find(blockData => blockData.id === blockId)
-        if (theActualBlock !== undefined) {
-          const extractedBlockData: PropsBlockData = {
-            id: theActualBlock.id as PropsBlockData['id'],
-            depth: (theActualBlock.depth ?? 'back') as PropsBlockData['depth'],
-            type: theActualBlock.type as PropsBlockData['type'],
-            content: theActualBlock.content as PropsBlockData['content'],
-            layout: theActualBlock.layout as PropsBlockData['layout'],
-            mobileLayout: theActualBlock.mobileLayout as PropsBlockData['mobileLayout'],
-            transitions: theActualBlock.transitions
-              ?.split(';')
-              .map(str => str
-                .trim()
-                .split(',')
-                .map(str => str.trim())
-                .map((val, pos) => {
-                  if (pos === 0) return val
-                  if (pos === 1 && val === undefined) return '600ms'
-                  if (val.match(/[0-9]$/gm)) return `${val}ms`
-                  return val
-                })
-              ) as PropsBlockData['transitions'],
-            mobileTransitions: theActualBlock.mobileTransitions
-              ?.split(';')
-              .map(str => str
-                .trim()
-                .split(',')
-                .map(str => str.trim())
-                .map((val, pos) => {
-                  if (pos === 0) return val
-                  if (pos === 1 && val === undefined) return '600ms'
-                  if (val.match(/[0-9]$/gm)) return `${val}ms`
-                  return val
-                })
-              ) as PropsBlockData['mobileTransitions'],
-            zIndex: theActualBlock.zIndex,
-            trackScroll: theActualBlock.trackScroll
-          }
-          fixedBlocksData.push(extractedBlockData)
-        }
-      })
-      return {
-        id: rawPageData.id,
-        showHeader: rawPageData.showHeader,
-        showNav: rawPageData.showNav,
-        headerLogoFill1: rawPageData.headerLogoFill1,
-        headerLogoFill2: rawPageData.headerLogoFill2,
-        headerCustomClass: rawPageData.headerCustomClass,
-        headerCustomCss: rawPageData.headerCustomCss,
-        headerNavItemsAlign: rawPageData.headerNavItemsAlign,
-        chapterName: rawPageData.chapterName,
-        isChapterHead: rawPageData.isChapterHead,
-        bgColor: rawPageData.bgColor,
+    const generalSettings: Partial<GeneralSettings> = {
+      // Ligne virtuelle dans l'écran représentant le seuil entre 2 pages
+      thresholdOffset: '60%',
+      bgColorTransitionDuration: '600ms',
+      // Combien de pages à l'avance faut il pour charger le contenu des blocs fixes
+      lazyLoadDistance: 2,
+      // N'est utile que si le scrllgngn est utilisé dans une div en overflow-y: scroll
+      // qui ne ferait pas toute la hauteur de l'écran (jamais quoi)
+      viewportHeight: '100vh',
+      // Utile si jamais on veut le mettre en cover d'article et qu'il reste le header du site,
+      // si on veut que les blocs fixes s'accrochent pile en dessous du header (~60px).
+      // Dans ce cas, je crois qu'il faut viewportHeight: 'calc(100vh - 60px)', mais bref
+      topOffset: 0,
+      // Ajouter une classe custom au header autre que .lm-article-header
+      // Par défaut, le header n'a pas de styles définis
+      headerCustomClass: undefined,
+      // Appliquer du css sur la classe custom plutot que .lm-article-header
+      headerCustomCss: undefined,
+      // left, center, right, par défaut: left
+      headerNavItemsAlign: 'center'
+    }
+    const pagesData: PropsPageData[] = [
+      // Une page simple sans les commentaires
+      {
+        showHeader: true,
+        showNav: true,
+        headerLogoFill1: 'black',
+        headerLogoFill2: 'rgb(0, 0, 0, .3)',
+        headerNavItemsAlign: 'center',
+        chapterName: 'Mon super chapitre',
+        isChapterHead: true,
+        bgColor: 'cornflowerblue',
         blocks: [{
           depth: 'scroll',
+          layout: 'right-half',
           type: 'html',
-          content: rawPageData.content,
-          layout: rawPageData.layout as PropsBlockData['layout'],
-          mobileLayout: rawPageData.mobileLayout as PropsBlockData['mobileLayout']
-        }, ...fixedBlocksData]
-      }
-    })
+          content: `<div style="
+            background-color: chocolate;
+            width: 100%;
+            height: 120vh;
+            padding: 16px;
+            border: 16px solid black;
+            box-sizing: border-box;
+            font-weight: 700;
+            font-family: var(--ff-marr-sans);">
+            Right half.
+          </div>`
+        }]
+      },
 
-    // const generalSettings: Partial<GeneralSettings> = {}
-    // const pagesData: PropsPageData[] = [{
-    //   bgColor: 'blueviolet',
-    //   blocks: [{
-    //     type: 'html',
-    //     depth: 'scroll',
-    //     layout: 'left-half',
-    //     content: `<div style="
-    //       background-color: coral;
-    //       width: 100%;
-    //       height: 80vh;
-    //       padding: 16px;
-    //       border: 16px solid black;
-    //       box-sizing: border-box;
-    //       font-weight: 700;
-    //       font-family: var(--ff-marr-sans);">
-    //       Left half.
-    //     </div>`
-    //   }, {
-    //     type: 'html',
-    //     depth: 'scroll',
-    //     layout: 'right-half',
-    //     content: `<div style="
-    //       background-color: coral;
-    //       width: 100%;
-    //       height: 80vh;
-    //       padding: 16px;
-    //       border: 16px solid black;
-    //       box-sizing: border-box;
-    //       font-weight: 700;
-    //       font-family: var(--ff-marr-sans);">
-    //       Right half.
-    //     </div>`
-    //   }, {
-    //     type: 'html',
-    //     depth: 'scroll',
-    //     layout: 'full-screen',
-    //     content: `<div style="
-    //       background-color: coral;
-    //       width: 100%;
-    //       height: 80vh;
-    //       padding: 16px;
-    //       border: 16px solid black;
-    //       box-sizing: border-box;
-    //       font-weight: 700;
-    //       font-family: var(--ff-marr-sans);">
-    //       Full screen
-    //     </div>`
-    //   }, /*{
-    //     type: 'html',
-    //     depth: 'back',
-    //     layout: 'right-half',
-    //     content: `<div style="
-    //       background-color: darkkhaki;
-    //       width: 100%;
-    //       padding: 16px;
-    //       border: 16px solid black;
-    //       box-sizing: border-box;
-    //       font-weight: 600; 
-    //       font-family: var(--ff-the-antiqua-b);">
-    //       Back / right half
-    //     </div>`
-    //   }, {
-    //     type: 'html',
-    //     depth: 'front',
-    //     layout: 'left-half',
-    //     content: `<div style="
-    //       background-color: darkkhaki;
-    //       width: 100%;
-    //       padding: 16px;
-    //       border: 16px solid black;
-    //       box-sizing: border-box;
-    //       font-weight: 600;
-    //       font-family: var(--ff-the-antiqua-b);">
-    //       Front / left half middle
-    //     </div>`
-    //   }, */{
-    //     type: 'html',
-    //     depth: 'front',
-    //     layout: 'full-screen',
-    //     content: `<div style="
-    //       background-color: darkkhaki;
-    //       /*margin-top: 80vh;*/
-    //       width: 100%;
-    //       padding: 16px;
-    //       border: 16px solid black;
-    //       box-sizing: border-box;
-    //       font-weight: 600;
-    //       font-family: var(--ff-the-antiqua-b);">
-    //       Front / full screen
-    //     </div>`
-    //   }]
-    // }]
+      // Une page avec le détail de chq propriété
+      {
+        showHeader: true,
+        showNav: true,
+        // Couleur principale du logo dans la nav
+        headerLogoFill1: 'white',
+        // Contreforme du logo (30% de l'opacité de la couleur principale c'est bien)
+        headerLogoFill2: 'rgb(255, 255, 255, .3)',
+        // Classes et css spécifiques du header, actifs uniquement quand la page est active
+        headerCustomClass: 'class-on-header-while-this-page-is-active',
+        headerCustomCss: '.class-on-header-while-this-page-is-active { opacity: 1; }',
+        headerNavItemsAlign: 'center',
+        // Nom du chapitre tel qu'affiché dans la nav
+        chapterName: 'Mon super chapitre',
+        // Cette page est-elle celle sur laquelle on atterrit
+        // quand on clique sur le chp dans la nav
+        isChapterHead: true,
+        // Couleur de fond DU SCRLLGNGN (pas de la page) lorsque la page est active
+        bgColor: 'blueviolet',
+        
+        // Liste des blocs présents dans la page.
+        blocks: [{
+          // Utile si on veut que le bloc soit présent sur plusieurs pages :
+          // On le définit avec toutes ses propriétés une première fois,
+          // puis on y fait référence dans d'autres pages (ULTÉRIEURES) en ne renseignant que l'id
+          id: 'le-nom-du-bloc',
+          // - scroll: le bloc scrolle dans la page. Les blocs de ce type s'ajoutent
+          //   les uns au dessus des autres, donc pas tellement d'intérêt d'en avoir plusieurs
+          // - back: le bloc est fixe et s'affiche à l'écran quand la page est active. Il est
+          //   en background, et donc passe en dessous des blocks scrollables
+          // - front: le bloc est fixe tout pareil, mais passe au dessus du contenu scrollable
+          depth: 'scroll',
+          // Utile pour arranger entre eux des blocs back ou entre eux des blocs front
+          // ne reflète pas le réel z-index dans la page, l'ordre des z-index final est :
+          // [
+          //  ...sortedZIndexesForBack,
+          //  ...sortedZIndexesForScroll,
+          //  ...sortedZIndexesForFront
+          // ]
+          zIndex: 2,
+          // left-half, right-half, full-screen, d'autres à venir, pas encore tout à fait
+          // satisfait de comment ça marche donc ça va sûrement bouger
+          layout: 'left-half',
+          // Si vide, c'est le layout qui s'applique, sinon, on applique un autre layout
+          // en dessous de 800px de largeur d'écran
+          mobileLayout: 'right-half',
+          // À n'utiliser que sur les blocs back ou front
+          // permet de paramétrer de quelle manière les blocs apparaissent quand ils deviennent
+          // actifs. Globalement inutile cependant : (
+          // Modèle: [[transition-name, duration-in-ms], ...]
+          // Genre: [['grow', 600]]
+          // Options: 'fade', 'grow', 'whirl', 'slide-up', 'right-open', 'left-open'
+          transitions: [],
+          // Même logique que pour mobileLayout
+          mobileTransitions: [],
+          // Type de contenu
+          // - html pour afficher directement le contenu de la propriété 'content',
+          // - module pour rendre le résultat du module js compatible, hébergé à l'adresse renseignée dans la propriété 'content'
+          // - more to come 😏
+          type: 'html',
+          // Utile si on a un type: 'module' et qu'on veut que le scrllgngn déclenche une update
+          // à chaque évènement scroll détecté sur la page courante
+          trackScroll: false,
+          // Le contenu à afficher, ou l'url du module
+          content: `<div style="
+            background-color: coral;
+            width: 100%;
+            height: 120vh;
+            padding: 16px;
+            border: 16px solid black;
+            box-sizing: border-box;
+            font-weight: 700;
+            font-family: var(--ff-marr-sans);">
+            Left half.
+          </div>`
+        }]
+      }
+    ]
 
     // Assign classes and styles
     const wrapperClasses = bem(props.className).block(this.clss)
