7c95490017df72aa6d7133ae415477b4f757f611
Maxime Fabas <fabas@lemonde.fr>
2023-02-09 18:12:08 +0100

    scrllgngn - take nice-color and html-placeholder generators to modules/utils

--- src/apps/Longform/index.tsx
+++ src/apps/Longform/index.tsx
@@ -3,18 +3,38 @@ import Scrollgneugneu, { Props as ScrollgngnProps, PropsPageData, PropsBlockData
 import appWrapper, { InjectedProps } from '../../modules/utils/app-wrapper-HOC'
 import bem from '../../modules/utils/bem'
 import './styles.scss'
-
 import {
   GeneralSettings,
   BlockData as BlockDataFromSheet,
   PageData as PageDataFromSheet
 } from '../types'
+import generateNiceColor from '../../modules/utils/generate-nice-color'
+import { generateContentPage } from '../../modules/utils/generate-html-placeholders'
 
 interface Props extends InjectedProps {}
 interface State {}
 
+const generalSettings: Partial<GeneralSettings> = {
+  thresholdOffset: '60%',
+  headerCustomCss: '.content-page > * { margin-bottom: 24px; } .content-page .title, .content-page .intertitle { font-weight: 600; }'
+}
+const pagesData: PropsPageData[] = [{
+  bgColor: generateNiceColor(),
+  blocks: [{
+    id: 'content-scroll-page',
+    depth: 'scroll',
+    type: 'html',
+    content: `<div style="background: white; font-family: var(--ff-marr-sans); padding: 64px; width: 400px;">${generateContentPage(4)}</div>`,
+  }]
+}, {
+  bgColor: generateNiceColor(),
+  blocks: [{
+    id: 'content-scroll-page'
+  }]
+}]
+
 class Longform extends Component<Props, State> {
-  static clss: string = 'sable-longform'
+  static clss: string = 'scrllgngn-longform'
   clss = Longform.clss
 
   /* * * * * * * * * * * * * * *
@@ -24,169 +44,75 @@ class Longform extends Component<Props, State> {
     const { props } = this
     const { sheetBase } = props
 
-    const generalSettings = sheetBase?.collection('general_settings').entries[0].value as GeneralSettings|undefined
-    const blocksData = sheetBase?.collection('blocks_data').value as BlockDataFromSheet[]|undefined
-    const rawPagesData = sheetBase?.collection('pages_data').value as PageDataFromSheet[]|undefined
-    const pagesData: PropsPageData[]|undefined = rawPagesData?.map(rawPageData => {
-      const fixedBlocksData: PropsBlockData[] = []
-      rawPageData.blocksIds?.split(',').map(name => {
-        const blockId = name.trim()
-        const theActualBlock = blocksData?.find(blockData => blockData.id === blockId)
-        if (theActualBlock !== undefined) {
-          const extractedBlockData: PropsBlockData = {
-            id: theActualBlock.id as PropsBlockData['id'],
-            depth: (theActualBlock.depth ?? 'back') as PropsBlockData['depth'],
-            type: theActualBlock.type as PropsBlockData['type'],
-            content: theActualBlock.content as PropsBlockData['content'],
-            layout: theActualBlock.layout as PropsBlockData['layout'],
-            mobileLayout: theActualBlock.mobileLayout as PropsBlockData['mobileLayout'],
-            transitions: theActualBlock.transitions
-              ?.split(';')
-              .map(str => str
-                .trim()
-                .split(',')
-                .map(str => str.trim())
-                .map((val, pos) => {
-                  if (pos === 0) return val
-                  if (pos === 1 && val === undefined) return '600ms'
-                  if (val.match(/[0-9]$/gm)) return `${val}ms`
-                  return val
-                })
-              ) as PropsBlockData['transitions'],
-            mobileTransitions: theActualBlock.mobileTransitions
-              ?.split(';')
-              .map(str => str
-                .trim()
-                .split(',')
-                .map(str => str.trim())
-                .map((val, pos) => {
-                  if (pos === 0) return val
-                  if (pos === 1 && val === undefined) return '600ms'
-                  if (val.match(/[0-9]$/gm)) return `${val}ms`
-                  return val
-                })
-              ) as PropsBlockData['mobileTransitions'],
-            zIndex: theActualBlock.zIndex,
-            trackScroll: theActualBlock.trackScroll
-          }
-          fixedBlocksData.push(extractedBlockData)
-        }
-      })
-      return {
-        id: rawPageData.id,
-        showHeader: rawPageData.showHeader,
-        showNav: rawPageData.showNav,
-        headerLogoFill1: rawPageData.headerLogoFill1,
-        headerLogoFill2: rawPageData.headerLogoFill2,
-        headerCustomClass: rawPageData.headerCustomClass,
-        headerCustomCss: rawPageData.headerCustomCss,
-        headerNavItemsAlign: rawPageData.headerNavItemsAlign,
-        chapterName: rawPageData.chapterName,
-        isChapterHead: rawPageData.isChapterHead,
-        bgColor: rawPageData.bgColor,
-        blocks: [{
-          depth: 'scroll',
-          type: 'html',
-          content: rawPageData.content,
-          layout: rawPageData.layout as PropsBlockData['layout'],
-          mobileLayout: rawPageData.mobileLayout as PropsBlockData['mobileLayout']
-        }, ...fixedBlocksData]
-      }
-    })
-
-    // const generalSettings: Partial<GeneralSettings> = {}
-    // const pagesData: PropsPageData[] = [{
-    //   bgColor: 'blueviolet',
-    //   blocks: [{
-    //     type: 'html',
-    //     depth: 'scroll',
-    //     layout: 'left-half',
-    //     content: `<div style="
-    //       background-color: coral;
-    //       width: 100%;
-    //       height: 80vh;
-    //       padding: 16px;
-    //       border: 16px solid black;
-    //       box-sizing: border-box;
-    //       font-weight: 700;
-    //       font-family: var(--ff-marr-sans);">
-    //       Left half.
-    //     </div>`
-    //   }, {
-    //     type: 'html',
-    //     depth: 'scroll',
-    //     layout: 'right-half',
-    //     content: `<div style="
-    //       background-color: coral;
-    //       width: 100%;
-    //       height: 80vh;
-    //       padding: 16px;
-    //       border: 16px solid black;
-    //       box-sizing: border-box;
-    //       font-weight: 700;
-    //       font-family: var(--ff-marr-sans);">
-    //       Right half.
-    //     </div>`
-    //   }, {
-    //     type: 'html',
-    //     depth: 'scroll',
-    //     layout: 'full-screen',
-    //     content: `<div style="
-    //       background-color: coral;
-    //       width: 100%;
-    //       height: 80vh;
-    //       padding: 16px;
-    //       border: 16px solid black;
-    //       box-sizing: border-box;
-    //       font-weight: 700;
-    //       font-family: var(--ff-marr-sans);">
-    //       Full screen
-    //     </div>`
-    //   }, /*{
-    //     type: 'html',
-    //     depth: 'back',
-    //     layout: 'right-half',
-    //     content: `<div style="
-    //       background-color: darkkhaki;
-    //       width: 100%;
-    //       padding: 16px;
-    //       border: 16px solid black;
-    //       box-sizing: border-box;
-    //       font-weight: 600; 
-    //       font-family: var(--ff-the-antiqua-b);">
-    //       Back / right half
-    //     </div>`
-    //   }, {
-    //     type: 'html',
-    //     depth: 'front',
-    //     layout: 'left-half',
-    //     content: `<div style="
-    //       background-color: darkkhaki;
-    //       width: 100%;
-    //       padding: 16px;
-    //       border: 16px solid black;
-    //       box-sizing: border-box;
-    //       font-weight: 600;
-    //       font-family: var(--ff-the-antiqua-b);">
-    //       Front / left half middle
-    //     </div>`
-    //   }, */{
-    //     type: 'html',
-    //     depth: 'front',
-    //     layout: 'full-screen',
-    //     content: `<div style="
-    //       background-color: darkkhaki;
-    //       /*margin-top: 80vh;*/
-    //       width: 100%;
-    //       padding: 16px;
-    //       border: 16px solid black;
-    //       box-sizing: border-box;
-    //       font-weight: 600;
-    //       font-family: var(--ff-the-antiqua-b);">
-    //       Front / full screen
-    //     </div>`
-    //   }]
-    // }]
+    // const generalSettings = sheetBase?.collection('general_settings').entries[0].value as GeneralSettings|undefined
+    // const blocksData = sheetBase?.collection('blocks_data').value as BlockDataFromSheet[]|undefined
+    // const rawPagesData = sheetBase?.collection('pages_data').value as PageDataFromSheet[]|undefined
+    // const pagesData: PropsPageData[]|undefined = rawPagesData?.map(rawPageData => {
+    //   const fixedBlocksData: PropsBlockData[] = []
+    //   rawPageData.blocksIds?.split(',').map(name => {
+    //     const blockId = name.trim()
+    //     const theActualBlock = blocksData?.find(blockData => blockData.id === blockId)
+    //     if (theActualBlock !== undefined) {
+    //       const extractedBlockData: PropsBlockData = {
+    //         id: theActualBlock.id as PropsBlockData['id'],
+    //         depth: (theActualBlock.depth ?? 'back') as PropsBlockData['depth'],
+    //         type: theActualBlock.type as PropsBlockData['type'],
+    //         content: theActualBlock.content as PropsBlockData['content'],
+    //         layout: theActualBlock.layout as PropsBlockData['layout'],
+    //         mobileLayout: theActualBlock.mobileLayout as PropsBlockData['mobileLayout'],
+    //         transitions: theActualBlock.transitions
+    //           ?.split(';')
+    //           .map(str => str
+    //             .trim()
+    //             .split(',')
+    //             .map(str => str.trim())
+    //             .map((val, pos) => {
+    //               if (pos === 0) return val
+    //               if (pos === 1 && val === undefined) return '600ms'
+    //               if (val.match(/[0-9]$/gm)) return `${val}ms`
+    //               return val
+    //             })
+    //           ) as PropsBlockData['transitions'],
+    //         mobileTransitions: theActualBlock.mobileTransitions
+    //           ?.split(';')
+    //           .map(str => str
+    //             .trim()
+    //             .split(',')
+    //             .map(str => str.trim())
+    //             .map((val, pos) => {
+    //               if (pos === 0) return val
+    //               if (pos === 1 && val === undefined) return '600ms'
+    //               if (val.match(/[0-9]$/gm)) return `${val}ms`
+    //               return val
+    //             })
+    //           ) as PropsBlockData['mobileTransitions'],
+    //         zIndex: theActualBlock.zIndex,
+    //         trackScroll: theActualBlock.trackScroll
+    //       }
+    //       fixedBlocksData.push(extractedBlockData)
+    //     }
+    //   })
+    //   return {
+    //     id: rawPageData.id,
+    //     showHeader: rawPageData.showHeader,
+    //     showNav: rawPageData.showNav,
+    //     headerLogoFill1: rawPageData.headerLogoFill1,
+    //     headerLogoFill2: rawPageData.headerLogoFill2,
+    //     headerCustomClass: rawPageData.headerCustomClass,
+    //     headerCustomCss: rawPageData.headerCustomCss,
+    //     headerNavItemsAlign: rawPageData.headerNavItemsAlign,
+    //     chapterName: rawPageData.chapterName,
+    //     isChapterHead: rawPageData.isChapterHead,
+    //     bgColor: rawPageData.bgColor,
+    //     blocks: [{
+    //       depth: 'scroll',
+    //       type: 'html',
+    //       content: rawPageData.content,
+    //       layout: rawPageData.layout as PropsBlockData['layout'],
+    //       mobileLayout: rawPageData.mobileLayout as PropsBlockData['mobileLayout']
+    //     }, ...fixedBlocksData]
+    //   }
+    // })
 
     // Assign classes and styles
     const wrapperClasses = bem(props.className).block(this.clss)
