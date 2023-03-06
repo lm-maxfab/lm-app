df6714444cd2b9bab8d9b395fcff1b6cd6420d3e
Elsa <edelmas@sempo10009.glm.lan>
2023-03-06 16:22:37 +0100

    BUILD - 42-2303-feministes - v0.0.0-stable-1

--- src/modules/layouts/Scrollgneugneu/BlockRenderer/index.tsx
+++ src/modules/layouts/Scrollgneugneu/BlockRenderer/index.tsx
@@ -3,12 +3,6 @@ import { BlockContext } from '..'
 import HtmlBlockRenderer from './HtmlBlockRenderer'
 import ModuleBlockRenderer from './ModuleBlockRenderer'
 
-import TextSequencer from '../../../components/TextSequencer'
-import { ContentData } from '../../../components/TextSequencer'
-
-import MessagesSequencer from '../../../components/MessagesSequencer'
-import { MessageData } from '../../../components/MessagesSequencer'
-
 type Props = {
   type?: 'module' | 'html'
   content?: string
@@ -29,52 +23,6 @@ export default class BlockRenderer extends Component<Props> {
       case undefined: return <HtmlBlockRenderer content={content} />
 
       case 'module':
-        // [WIP] remove this
-        const sequencerDetectionRegexp = /^\[\[SEQUENCER\]\]/
-
-        if (content?.match(sequencerDetectionRegexp)) {
-          const sequencerPropsStr = content
-            .replace(sequencerDetectionRegexp, '')
-            .trim()
-          const sequencerProps = {
-            tempo: 60,
-            content: "Lorem ipsum dolor sit amet.",
-            scrollInfo: "Défilez pour continuer"
-          }
-          let sequencerType = "text"
-
-          try {
-            console.log(sequencerPropsStr)
-            const parsed = JSON.parse(sequencerPropsStr)
-            console.log(parsed)
-            if (parsed === null) break;
-            if (typeof parsed !== 'object') break;
-            if (Array.isArray(parsed)) break;
-            if (typeof parsed.tempo === 'number') sequencerProps.tempo = parsed.tempo
-            if (typeof parsed.scrollInfo === 'string') sequencerProps.scrollInfo = parsed.scrollInfo
-            if (parsed.content != '') sequencerProps.content = parsed.content
-            if (parsed.type === 'messages') sequencerType = 'messages'
-          } catch (err) { console.log(err) }
-
-          if (sequencerType === "text") {
-            return <TextSequencer
-              content={sequencerProps.content as unknown as ContentData}
-              scrollInfo={sequencerProps.scrollInfo}
-              tempo={sequencerProps.tempo}
-              active={context?.page === 0}
-            />
-          }
-
-          if (sequencerType === "messages") {
-            return <MessagesSequencer
-              content={sequencerProps.content as unknown as MessageData[]}
-              scrollInfo={sequencerProps.scrollInfo}
-              tempo={sequencerProps.tempo}
-              active={context?.page === 0}
-            />
-          }
-        }
-
         return <ModuleBlockRenderer
           url={content}
           context={context}
