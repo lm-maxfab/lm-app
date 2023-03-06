0566112ac5e0bcba3e3876e9bd5c9d80d2f9cc67
Elsa <edelmas@sempo10009.glm.lan>
2023-03-06 16:20:17 +0100

    wip format féministes 8 mars

---
+++ src/apps/Snippet/index.tsx
@@ -0,0 +1,91 @@
+import { Component, JSX, VNode, createRef } from 'preact'
+import appWrapper, { InjectedProps } from '../../modules/utils/app-wrapper-HOC'
+import styles from './styles.module.scss'
+
+interface SnippetData {
+  quote: VNode | string
+  name: string
+  bio: VNode | string
+  portrait: string
+}
+
+interface Props extends InjectedProps { }
+interface State { }
+
+class Snippet extends Component<Props, State> {
+  container: any
+  id: number
+
+  constructor() {
+    super()
+
+    this.container = createRef()
+    this.id = 1
+  }
+
+  componentDidMount(): void {
+    const parent = this.container.current.closest('.lm-app-root')
+    const parentClassList = parent.classList.value.split(' ')
+    const snippetClass = parentClassList.find((el: string) => el.startsWith('lm-snippet'))
+
+    this.id = snippetClass.slice(-1)
+  }
+
+  /* * * * * * * * * * * * * * *
+   * RENDER
+   * * * * * * * * * * * * * * */
+  render(): JSX.Element {
+
+
+    const { props } = this
+
+
+    const data = props.sheetBase?.collection('data').value[this.id - 1] as unknown as SnippetData;
+
+    const wrapperClasses = [
+      styles['wrapper']
+    ]
+
+    const quoteClasses = [
+      styles['quote']
+    ]
+
+    const authorClasses = [
+      styles['author']
+    ]
+
+    const nameClasses = [
+      styles['name']
+    ]
+
+    const bioClasses = [
+      styles['bio']
+    ]
+
+    const portraitClasses = [
+      styles['portrait']
+    ]
+
+    const portraitUrl = `https://assets-decodeurs.lemonde.fr/redacweb/42-2303-feministes-medias/portrait-${this.id}.png`
+
+    return <div ref={this.container} className={wrapperClasses.join(' ')}>
+      <p className={quoteClasses.join(' ')}>« {data.quote} »</p>
+
+      <div className={authorClasses.join(' ')}>
+        <img
+          src={portraitUrl}
+          alt={data.name as string}
+          className={portraitClasses.join(' ')}
+        />
+
+        <div>
+          <p className={nameClasses.join(' ')}>{data.name}</p>
+          <p className={bioClasses.join(' ')}>{data.bio}</p>
+        </div>
+      </div>
+    </div>
+  }
+}
+
+export type { Props, Snippet }
+export default appWrapper(Snippet)
--- src/apps/index.tsx
+++ src/apps/index.tsx
@@ -1,21 +1,19 @@
 import { AppNodeMap } from '../modules/utils/render-app'
-import LongformOf from './Longform'
-import Sidenote from './Sidenote'
-import Footer from './Footer'
+import Snippet from './Snippet'
 
 const appsNodes: AppNodeMap[] = [
-  { app: Footer, selector: '.pedocrim-footer' },
-  { app: Sidenote, selector: '.pedocrim-sidenote' },
-  { app: LongformOf('cover-article-1'), selector: '.pedocrim-cover-article-1' },
-  { app: LongformOf('cover-article-2'), selector: '.pedocrim-cover-article-2' },
-  { app: LongformOf('cover-article-3'), selector: '.pedocrim-cover-article-3' },
-  { app: LongformOf('cover-article-4'), selector: '.pedocrim-cover-article-4' },
-  { app: LongformOf('cover-article-5'), selector: '.pedocrim-cover-article-5' },
-  { app: LongformOf('cover-article-6'), selector: '.pedocrim-cover-article-6' },
-  { app: LongformOf('cover-article-7'), selector: '.pedocrim-cover-article-7' },
-  { app: LongformOf('cover-article-8'), selector: '.pedocrim-cover-article-8' },
-  { app: LongformOf('cover-article-9'), selector: '.pedocrim-cover-article-9' },
-  { app: LongformOf('cover-article-10'), selector: '.pedocrim-cover-article-10' }
+  { app: Snippet, selector: '.lm-snippet-1' },
+  { app: Snippet, selector: '.lm-snippet-2' },
+  { app: Snippet, selector: '.lm-snippet-3' },
+  { app: Snippet, selector: '.lm-snippet-4' },
+  { app: Snippet, selector: '.lm-snippet-5' },
+  { app: Snippet, selector: '.lm-snippet-6' },
+  { app: Snippet, selector: '.lm-snippet-7' },
+  { app: Snippet, selector: '.lm-snippet-8' },
+  { app: Snippet, selector: '.lm-snippet-9' },
+  { app: Snippet, selector: '.lm-snippet-10' },
+  { app: Snippet, selector: '.lm-snippet-11' },
+  { app: Snippet, selector: '.lm-snippet-12' },
 ]
 
 export default appsNodes
