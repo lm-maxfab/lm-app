48e7aabd5bf57e9470ce83f7b975f116c080bdd8
Elsa <edelmas@sempo10009.glm.lan>
2023-03-09 12:00:53 +0100

    BUILD - 41-2303-pedocrim - v0.0.0-stable-36

--- src/modules/components/MessagesSequencer/index.tsx
+++ src/modules/components/MessagesSequencer/index.tsx
@@ -38,6 +38,7 @@ class MessagesSequencer extends Component<Props, State> {
     ]
 
     if (props.customClass != null) wrapperClasses.push(props.customClass)
+    if (props.active === false) wrapperClasses.push(`${messagesClass}_wrapper--inactive`)
 
     const messagesClasses = [
       messagesClass,
@@ -60,9 +61,12 @@ class MessagesSequencer extends Component<Props, State> {
               styles['message']
             ]
 
-            containerClasses.push(`message-container--${index}`)
+            containerClasses.push(`${messageClass}_container--${index}`)
 
-            if (start < step) containerClasses.push(styles['message-container--visible'])
+            if (start < step) { 
+              containerClasses.push(styles['message-container--visible']) 
+              containerClasses.push(`${messageClass}_container--visible`) 
+            }
 
             if (message.type != '' && message.type != undefined) {
               containerClasses.push(`${messageClass}_container--${message.type}`)
@@ -70,10 +74,6 @@ class MessagesSequencer extends Component<Props, State> {
 
             return (
               <div class={containerClasses.join(' ')}>
-                <svg width="12" height="14" viewBox="0 0 12 14" >
-                  <path fill-rule="evenodd" clip-rule="evenodd" d="M3 0.530407C3 5.4199 5.49121 8.83875 10.4737 10.787L10.8095 10.9147C11.3275 11.1062 11.5922 11.6814 11.4007 12.1994C11.3289 12.3936 11.1986 12.5608 11.0277 12.6778C9.26716 13.883 4.42005 13.8092 0.5 14.0003V7.50027V3.54372C0.5 2.06868 1.55032 0.802711 3 0.530407V0.530407Z" />
-                </svg>
-
                 <p class={textClasses.join(' ')}>{message.text}</p>
               </div>
             )
